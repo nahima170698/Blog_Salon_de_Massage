3b9eaead35751a57266cd30b802ae7286b845b34
Nahima <nahima17.l@gmail.com>
2023-07-05 10:29:04 +0400

    update blog_salon_de_massage

--- index.php
+++ index.php
@@ -1,30 +1,89 @@
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Salon_de_massage</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
     <link rel="stylesheet" href="index.css">
 
 </head>
+
 <body>
-<!--Navbar-->
-<header>
-<nav>
-    <img src="massage_logo_header-1 (1).png" alt="MASSAGE">
-    <a class="nav">HOMEPAGE</a>
-        <a class="nav">ABOUT US</a>
-        <a class="nav">PAGE & PROJECTS</a>
-        <a class="nav">BLOG</a>
-        <button>CONTACT</button>
-</nav>
-</header>  
+    <!--Navbar-->
+    <header>
+        <nav id="hampter">
+            <img src="massage_logo_header-1 (1).png" alt="MASSAGE">
+            <!-- Example split danger button -->
+            <li id="ensemble">
+                <div class="btn-group">
+                    <button type="button" class="btn sdm">HOMEPAGE</button>
+                    <button type="button" class="btn dropdown-toggle dropdown-toggle-split sdm" data-bs-toggle="dropdown" aria-expanded="false">
+                        <span class="visually-hidden">Toggle Dropdown</span>
+                    </button>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="#">HOMEPAGE 1</a></li>
+                        <li><a class="dropdown-item" href="#">HOMEPAGE 2</a></li>
+                        <li><a class="dropdown-item" href="#">HOMEPAGE 3</a></li>
+                    </ul>
+                </div>
+                <div class="btn-group">
+                    <button type="button" class="btn sdm">ABOUT US</button>
+                    <button type="button" class="btn dropdown-toggle dropdown-toggle-split" data-bs-toggle="dropdown" aria-expanded="false">
+                        <span class="visually-hidden">Toggle Dropdown</span>
+                    </button>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="#">ABOUT US 1</a></li>
+                        <li><a class="dropdown-item" href="#">ABOUT US 2</a></li>
+                    </ul>
+                </div>
+                <div class="btn-group">
+                    <button type="button" class="btn sdm">PAGES & PROJECTS</button>
+                    <button type="button" class="btn dropdown-toggle dropdown-toggle-split" data-bs-toggle="dropdown" aria-expanded="false">
+                        <span class="visually-hidden">Toggle Dropdown</span>
+                    </button>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item lien" href="#">OUR TEAM</a></li>
+                        <li><a class="dropdown-item lien" href="#">GALLERY</a></li>
+                        <li><a class="dropdown-item lien" href="#">SINGLE SERVICE</a></li>
+                        <li><a class="dropdown-item lien" href="#">SINGLE SERVICE WIDTH SIDEBAR</a></li>
+                        <li><a class="dropdown-item lien" href="#">PORTEFOLLIO MODULE</a></li>
+                        <li><a class="dropdown-item lien" href="#">LIST OF BLURBS</a></li>
+                        <li><a class="dropdown-item lien" href="#">LIST OF ICONS</a></li>
+                        <li><a class="dropdown-item lien" href="#">404 PAGES</a></li>
+                    </ul>
+                </div>
+                <div class="btn-group">
+                    <button type="button" class="btn sdm">BLOG</button>
+                    <button type="button" class="btn dropdown-toggle dropdown-toggle-split" data-bs-toggle="dropdown" aria-expanded="false">
+                        <span class="visually-hidden">Toggle Dropdown</span>
+                    </button>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item lien" href="#">BLOG PAGE</a></li>
+                        <li><a class="dropdown-item lien" href="#">BLOG POST WITH RIGHT SIDEBAR</a></li>
+                        <li><a class="dropdown-item lien" href="#">BLOG POST WITH LEFT SIDEBAR</a></li>
+                        <li><a class="dropdown-item lien" href="#">BLOG POST FULL WIDTH</a></li>
+                    </ul>
+                    <button type="button" class="buttoncolor sdm">CONTACT</button>
+                </div>
+            </li>
+        </nav>
+        <div></div>
+        <div id="deux"><img src="icon05.png" id="icon5" alt=""></div>
+        <h1>
+        WELCOME
+        </h1>
+        <strong id="sec">IN BEAUTY PARADISE</strong>
+        
+    </header>
+    
+    <main>
+        <section class="section1"></section>
+    </main>
 
-<main>
-    <section1 class="section1"></section1>
-</main>
 
-
-
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
 </body>
+
 </html>
