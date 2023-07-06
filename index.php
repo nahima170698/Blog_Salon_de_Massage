5ad164f38de380e49ec68324c96bd218aa2a002d
Nahima <nahima17.l@gmail.com>
2023-07-06 13:03:04 +0400

    mise à jour

--- index.php
+++ index.php
@@ -1,89 +1,334 @@
-<!DOCTYPE html>
-<html lang="fr">
+<html lang="en">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Salon_de_massage</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
-    <link rel="stylesheet" href="index.css">
+    <title>Blog_Salon_de_Massage</title>
 
+    <!--Police-->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
+    <!--Logo-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <link rel="stylesheet" href="index.css">
 </head>
 
 <body>
-    <!--Navbar-->
-    <header>
-        <nav id="hampter">
-            <img src="massage_logo_header-1 (1).png" alt="MASSAGE">
-            <!-- Example split danger button -->
-            <li id="ensemble">
-                <div class="btn-group">
-                    <button type="button" class="btn sdm">HOMEPAGE</button>
-                    <button type="button" class="btn dropdown-toggle dropdown-toggle-split sdm" data-bs-toggle="dropdown" aria-expanded="false">
-                        <span class="visually-hidden">Toggle Dropdown</span>
-                    </button>
-                    <ul class="dropdown-menu">
-                        <li><a class="dropdown-item" href="#">HOMEPAGE 1</a></li>
-                        <li><a class="dropdown-item" href="#">HOMEPAGE 2</a></li>
-                        <li><a class="dropdown-item" href="#">HOMEPAGE 3</a></li>
-                    </ul>
-                </div>
-                <div class="btn-group">
-                    <button type="button" class="btn sdm">ABOUT US</button>
-                    <button type="button" class="btn dropdown-toggle dropdown-toggle-split" data-bs-toggle="dropdown" aria-expanded="false">
-                        <span class="visually-hidden">Toggle Dropdown</span>
-                    </button>
-                    <ul class="dropdown-menu">
-                        <li><a class="dropdown-item" href="#">ABOUT US 1</a></li>
-                        <li><a class="dropdown-item" href="#">ABOUT US 2</a></li>
-                    </ul>
-                </div>
-                <div class="btn-group">
-                    <button type="button" class="btn sdm">PAGES & PROJECTS</button>
-                    <button type="button" class="btn dropdown-toggle dropdown-toggle-split" data-bs-toggle="dropdown" aria-expanded="false">
-                        <span class="visually-hidden">Toggle Dropdown</span>
-                    </button>
-                    <ul class="dropdown-menu">
-                        <li><a class="dropdown-item lien" href="#">OUR TEAM</a></li>
-                        <li><a class="dropdown-item lien" href="#">GALLERY</a></li>
-                        <li><a class="dropdown-item lien" href="#">SINGLE SERVICE</a></li>
-                        <li><a class="dropdown-item lien" href="#">SINGLE SERVICE WIDTH SIDEBAR</a></li>
-                        <li><a class="dropdown-item lien" href="#">PORTEFOLLIO MODULE</a></li>
-                        <li><a class="dropdown-item lien" href="#">LIST OF BLURBS</a></li>
-                        <li><a class="dropdown-item lien" href="#">LIST OF ICONS</a></li>
-                        <li><a class="dropdown-item lien" href="#">404 PAGES</a></li>
-                    </ul>
-                </div>
-                <div class="btn-group">
-                    <button type="button" class="btn sdm">BLOG</button>
-                    <button type="button" class="btn dropdown-toggle dropdown-toggle-split" data-bs-toggle="dropdown" aria-expanded="false">
-                        <span class="visually-hidden">Toggle Dropdown</span>
-                    </button>
-                    <ul class="dropdown-menu">
-                        <li><a class="dropdown-item lien" href="#">BLOG PAGE</a></li>
-                        <li><a class="dropdown-item lien" href="#">BLOG POST WITH RIGHT SIDEBAR</a></li>
-                        <li><a class="dropdown-item lien" href="#">BLOG POST WITH LEFT SIDEBAR</a></li>
-                        <li><a class="dropdown-item lien" href="#">BLOG POST FULL WIDTH</a></li>
-                    </ul>
-                    <button type="button" class="buttoncolor sdm">CONTACT</button>
-                </div>
-            </li>
-        </nav>
-        <div></div>
-        <div id="deux"><img src="icon05.png" id="icon5" alt=""></div>
-        <h1>
-        WELCOME
-        </h1>
-        <strong id="sec">IN BEAUTY PARADISE</strong>
-        
-    </header>
-    
-    <main>
-        <section class="section1"></section>
-    </main>
-    
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+    <header id="navbar">
+        <nav id="menu">
+            <ul>
+                <ul class="linkmenu"><a class="lien btn" href="#"><img id="logo" src="https://massage-theme.richardpruzek.com/wp-content/uploads/2020/04/massage_logo_header-1.png"></a></ul>
+            </ul>
+            <ul id="lienentête">
+
+                <ul>
+                    <a class="lien btn" href="#">HOMEPAGE<i class="fa-solid fa-chevron-down fa-2xs" style="color: #abb8c3;"></i></a>
+                </ul>
+                <ul>
+                    <a class="lien btn" href="#">ABOUT US<i class="fa-solid fa-chevron-down fa-2xs" style="color: #abb8c3;"></i></a>
+
+                </ul>
+                <ul>
+                    <a class="lien btn" href="#">PAGES & PROJECTS<i class="fa-solid fa-chevron-down fa-2xs" style="color: #abb8c3;"></i> </a>
+
+                </ul>
+                <ul>
+                    <a class="lien btn" href="#">BLOG<i class="fa-solid fa-chevron-down fa-2xs" style="color: #abb8c3;"></i> </a>
+
+                </ul>
+                <ul>
+                    <button id="bouton_nul" class="btn">CONTACT</button>
+
+                </ul>
+
+            </ul>
+
+        </nav>
+
+    </header>
+
+    <main>
+        <section id="sect1">
+            <div class="section">
+                <!-- WELCOME IN BEAUTY PARADISE -->
+                <img src="https://massage-theme.richardpruzek.com/wp-content/uploads/2020/02/icon05.png" alt="" id="logo2">
+                <h1 id="titre">WELCOME <br>
+                    <strong>IN BEAUTY PARADISE</strong>
+                </h1>
+                <p>In publishing and graphic design, lorem <br>
+                    ipsum is common to cry</p>
+                <button>BOOK A MASSAGE</button>
+            </div>
+        </section>
+
+        <section id="sect2">
+
+            <!-- what we do OUR SERVICES -->
+            <div>
+                <div class="services">
+                    <h3 class="serv">what we do</h3>
+                    <h2>OUR SERVICES</h2>
+                    <!-- <img src="https://massage-theme.richardpruzek.com/wp-content/uploads/2020/02/massage12.jpg" alt="" id="logo3"> image de fond -->
+                </div>
+                <div class="proffessional">
+                    <p>In publishing and graphic design, lorem ipsum is common placeholder text used to demonstrate the graphic elements of a document or</p>
+                    <div>
+                        <img src="https://massage-theme.richardpruzek.com/wp-content/uploads/2020/02/massage33.jpg" alt="">
+                        <h3>PROFFESSIONAL MASSAGE</h3>
+                        <p></p>
+                    </div>
+                </div>
+                <div>
+                    <p></p>
+                    <p></p>
+                    <div>
+                        <img src="https://massage-theme.richardpruzek.com/wp-content/uploads/2020/02/massage32.jpg" alt="">
+                        <h3>PROFFESSIONAL MANICURE </h3>
+                        <p></p>
+                    </div>
+                </div>
+                <div>
+                    <div>
+                        <img src="https://massage-theme.richardpruzek.com/wp-content/uploads/2020/02/massage33.jpg" alt="">
+                        <h3>MAKE-UP ARTIST</h3>
+                        <p></p>
+                    </div>
+                </div>
+            </div>
+
+            <!-- what makes us unique WHY TO CHOOSE OUR SALON -->
+            <div>
+                <p></p>
+                <p></p>
+                <div>
+                    <ul>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                    </ul>
+                    <ul>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                    </ul>
+                    <img src="https://massage-theme.richardpruzek.com/wp-content/uploads/2020/02/massage18.png" alt="">
+                </div>
+            </div>
+
+            <!-- Bar avec background color pink-->
+            <div>
+                <div>
+                    <h1></h1>
+                    <button></button>
+                </div>
+            </div>
+
+            <!-- Introducing Massage Studio -->
+            <div>
+                <div>
+                    <h2></h2>
+                    <!-- trait rose -->
+                    <hr>
+                    <p></p>
+                </div>
+                <!-- <img src="https://massage-theme.richardpruzek.com/wp-content/uploads/2020/02/massage35-1.jpg" alt=""> -->
+            </div>
+
+            <!-- 5 cartes en slide  -->
+            <div>
+                <div>
+                    <img src="https://massage-theme.richardpruzek.com/wp-content/uploads/2020/02/icon08.png" alt="">
+                    <h2></h2>
+                    <p></p>
+                </div>
+                <div>
+                    <img src="https://massage-theme.richardpruzek.com/wp-content/uploads/2020/02/icon01.png" alt="">
+                    <h2></h2>
+                    <p></p>
+                </div>
+                <div>
+                    <img src="https://massage-theme.richardpruzek.com/wp-content/uploads/2020/02/icon03.png" alt="">
+                    <h2></h2>
+                    <p></p>
+                </div>
+                <div>
+                    <img src="https://massage-theme.richardpruzek.com/wp-content/uploads/2020/02/icon04.png" alt="">
+                    <h2></h2>
+                    <p></p>
+                </div>
+                <div>
+                    <img src="https://massage-theme.richardpruzek.com/wp-content/uploads/2020/02/icon10.png" alt="">
+                    <h2></h2>
+                    <p></p>
+                </div>
+            </div>
+
+            <!-- You Deserve Your Time! -->
+            <div>
+                <!-- <img src="https://massage-theme.richardpruzek.com/wp-content/uploads/2020/02/massage39.jpg" alt=""> -->
+                <div>
+                    <h2></h2>
+                    <p></p>
+                </div>
+            </div>
+
+            <!-- magazine OUR BEAUTY BLOG 3 cartes -->
+            <div>
+                <div>
+                    <img src="https://massage-theme.richardpruzek.com/wp-content/uploads/2020/02/massage21-400x250.jpg" alt="">
+                    <h3></h3>
+                    <p></p>
+                    <button></button>
+                </div>
+                <div>
+                    <img src="https://massage-theme.richardpruzek.com/wp-content/uploads/2020/02/massage20-400x250.jpg" alt="">
+                    <h3></h3>
+                    <p></p>
+                    <button></button>
+                </div>
+                <div>
+                    <img src="https://massage-theme.richardpruzek.com/wp-content/uploads/2020/02/massage07-400x250.jpg" alt="">
+                    <h3></h3>
+                    <p></p>
+                    <button></button>
+                </div>
+            </div>
+
+            <!-- meet our team OUR AWESOME MASSEURS 4 cartes -->
+            <div>
+
+                <!-- Carte -->
+                <div>
+                    <div>
+                        <img src="https://massage-theme.richardpruzek.com/wp-content/uploads/2020/02/massage18.png" alt="">
+                    </div>
+                    <h2></h2>
+                    <p></p>
+                    <div>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                    </div>
+                </div>
+                <div>
+                    <div>
+                        <img src="https://massage-theme.richardpruzek.com/wp-content/uploads/2020/02/massage37-1.png" alt="">
+                    </div>
+                    <h2></h2>
+                    <p></p>
+                    <div>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                    </div>
+                </div>
+                <div>
+                    <div>
+                        <img src="https://massage-theme.richardpruzek.com/wp-content/uploads/2020/02/massage35-1-1.png" alt="">
+                    </div>
+                    <h2></h2>
+                    <p></p>
+                    <div>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                    </div>
+                </div>
+                <div>
+                    <div>
+                        <img src="https://massage-theme.richardpruzek.com/wp-content/uploads/2020/02/massage38.png" alt="">
+                    </div>
+                    <h2></h2>
+                    <p></p>
+                    <div>
+                        <li></li>
+                        <li></li>
+                    </div>
+                </div>
+
+            </div>
+
+        </section>
+
+    </main>
+
+
+    <footer>
+        <div id="margedufooter">
+            <div id="fin_page">
+                <ul class="uldebas">
+                    <li>
+                        <h3>MASSAGE TYPES</h3>
+                    </li>
+                    <li><a href="">Sport Massage <i class="fa-solid fa-arrow-right-long" style="color: transparent;"></i></a></li>
+                    <li><a href="">Baby Massage <i class="fa-solid fa-arrow-right-long" style="color: transparent;"></i></a></li>
+                    <li><a href="">Thai Massage <i class="fa-solid fa-arrow-right-long" style="color: transparent;"></i></a></li>
+                    <li><a href="">Classic Massage <i class="fa-solid fa-arrow-right-long" style="color: transparent;"></i></a></li>
+                    <li><a href="">Physiotherapy <i class="fa-solid fa-arrow-right-long" style="color: transparent;"></i></a></li>
+                    <li><a href="">Physiotherapy <i class="fa-solid fa-arrow-right-long" style="color: transparent;"></i></a></li>
+                </ul>
+
+                <ul class="uldebas">
+                    <li>
+                        <h3>ABOUT US</h3>
+                    </li>
+                    <li><a href="">Our Masseurs <i class="fa-solid fa-arrow-right-long" style="color: transparent;"></i></a></li>
+                    <li><a href=""> Our Clients <i class="fa-solid fa-arrow-right-long" style="color: transparent;"></i></a></li>
+                    <li><a href="">Our Values <i class="fa-solid fa-arrow-right-long" style="color: transparent;"></i></a></li>
+                    <li><a href="">Photogallery <i class="fa-solid fa-arrow-right-long" style="color: transparent;"></i></a></li>
+                    <li><a href="">Our Philosophy <i class="fa-solid fa-arrow-right-long" style="color: transparent;"></i></a></li>
+                    <li><a href="">Our 2020 Goals <i class="fa-solid fa-arrow-right-long" style="color: transparent;"></i></a></li>
+                </ul>
+
+                <ul class="uldebas">
+                    <li>
+                        <h3>CONTACT US</h3>
+                    </li>
+                    <li class="contacts"><img class="logobas" src="https://massage-theme.richardpruzek.com/wp-content/uploads/2020/02/icon11.png" alt="">
+                        <p>+49 8568 523 525 <br>
+                            +43 8568 856 235</p>
+                    </li>
+                    <li class="contacts"><img class="logobas" src="https://massage-theme.richardpruzek.com/wp-content/uploads/2020/02/icon07.png" alt="">
+                        <p>info@massage.com <br>
+                            hello@myspa.de</p>
+                    </li>
+                    <li class="contacts"><img class="logobas" src="https://massage-theme.richardpruzek.com/wp-content/uploads/2020/02/icon09.png" alt="">
+                        <p>info@massage.com <br>
+                            helo@myspa.de</p>
+                    </li>
+                </ul>
+
+                <ul class="uldebas">
+                    <li>
+                        <h3>SUBSCRIBE</h3>
+                    </li>
+                    <li><input type="text" placeholder="NAME"></li>
+                    <li><input type="email" placeholder="EMAIL"></li>
+                    <li><input type="submit" placeholder="SUBSCRIBE"></li>
+                </ul>
+            </div>
+
+            <div id="copyright">
+                <!-- Copyright -->
+                <div>
+                    <p>Copyright Massage Humburg</p>
+                    <p>Created by an awesome web agency</p>
+                </div>
+                <!-- logo RÃ©seaux Sociaux -->
+                <div>
+                    <i class="fa-brands fa-lg fa-facebook-f" style="color: #ffffff;"></i>
+                    <i class="fa-brands fa-lg fa-twitter" style="color: #ffffff;"></i>
+                    <i class="fa-brands fa-lg fa-instagram" style="color: #ffffff;"></i>
+                    <i class="fa-brands fa-lg fa-youtube" style="color: #ffffff;"></i>
+                </div>
+            </div>
+
+        </div>
+
+    </footer>
 </body>
 
 </html>
